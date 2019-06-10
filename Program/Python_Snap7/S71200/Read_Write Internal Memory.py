61604f362691f3f9c6e16a746a58a5997a7ea7d6
LYU <lvxudong@palmarytech.com>
2019-06-10 18:09:18 +0800

    4th commit
    修改Python程序

--- Program/Python_Snap7/S71200/Read_Write Internal Memory.py
+++ Program/Python_Snap7/S71200/Read_Write Internal Memory.py
@@ -1,5 +1,4 @@
 import snap7.client as client
-import snap7
 from snap7.util import *
 
 
@@ -9,10 +8,10 @@ def write_output(dev, byte, bit, cmd):
     dev.write_area(0x82, 0, byte, data)
 
 
-s71200 = snap7.client.Client()
-s71200.connect('192.168.100.2', 0, 1)
+s71200 = client.Client()
+s71200.connect('192.168.2.110', 0, 1)
 connectionIsOk = s71200.get_connected()
 print(connectionIsOk)
 
-write_output(s71200, 0, 0, True)
+#write_output(s71200, 0, 0, True)
 

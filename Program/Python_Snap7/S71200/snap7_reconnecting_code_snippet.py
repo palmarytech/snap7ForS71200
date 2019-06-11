ac6b1b1bb5cf168e3e3db909b745fef168308c66
LYU <lvxudong@palmarytech.com>
2019-06-11 18:22:20 +0800

    7th commit
    增减通过Python实现S7-1200输出控制

--- Program/Python_Snap7/S71200/snap7_reconnecting_code_snippet.py
+++ Program/Python_Snap7/S71200/snap7_reconnecting_code_snippet.py
@@ -4,7 +4,6 @@ import snap7
 from snap7.snap7exceptions import Snap7Exception
 from snap7.util import *
 
-
 def connect(device, ip, rack, slot):
     while True:
         # check connection
@@ -17,16 +16,20 @@ def connect(device, ip, rack, slot):
             pass
         sleep(5)
 
+def ReadOutput(dev):
+    data = dev.read_area(0x82,0,0,1)
+    binary_list = [int(x) for x in bin(data[0])[2:]]
+    print(binary_list)
 
-plc = snap7.client.Client()
-connect(plc, '10.10.55.109', 0, 1)
-while True:
+def main():
+    s71200 = snap7.client.Client()
+    connect(s71200, '192.168.2.110', 0, 1)
+    while True:
+        try:
+            ReadOutput(s71200)
+            sleep(5)
+        except Snap7Exception as e:
+            connect(s71200, '192.168.2.110', 0, 1)
 
-    try:
-
-        pass
-
-    except Snap7Exception as e:
-
-        connect(plc, '10.10.55.109', 0, 1)
-        # break
+if __name__ == '__main__':
+    main()

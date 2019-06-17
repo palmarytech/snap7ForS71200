8442e2f6dde552f1335b7a3fb11b95136f0404f1
LYU <lvxudong@palmarytech.com>
2019-06-17 19:09:49 +0800

    17th commit
    增加DB块写操作的Python代码

--- Program/Python_Snap7/S71200/Mapping and Reading Datablocks.py
+++ Program/Python_Snap7/S71200/Mapping and Reading Datablocks.py
@@ -5,11 +5,11 @@ from snap7.util import *
 
 db = \
 """
-DB001 Real 0.0
-DB002 Bool 4.0
-DB003 Int 6.0
-DB004 String 8.0
-DB005 Real 264.0
+DB001 Real 0.0 1.2
+DB002 Bool 4.0 1
+DB003 Int 6.0 12
+DB004 String 8.0 'iLearning'
+DB005 Real 264.0 15.6
 """
 
 offsets = { "Bool":2,"Int": 2,"Real":4,"DInt":6,"String":256}
@@ -52,8 +52,30 @@ def DBRead(dev,db_num,db_len,db_items):
             value = get_string(data, offset, 256)
 
         obj.__setattr__(item['name'], value)
-
     return obj
+
+def DBWrite(dev, db_num, db_len, db_items):
+    data = dev.read_area(0x84, db_num, 0, db_len)
+    for item in db_items:
+        value = None
+        offset = int(item['bytebit'].split('.')[0])
+
+        if item['datatype']=='Real':
+            db_value = float(item['db_value'])
+            set_real(data,offset, db_value)
+
+        if item['datatype']=='Bool':
+            db_value = bool(item['db_value'])
+            bit =int(item['bytebit'].split('.')[1])
+            set_bool(data,offset,bit,db_value)
+
+        if item['datatype']=='Int':
+            db_value = int(item['db_value'])
+            set_int(data, offset ,db_value)
+
+        if item['datatype']=='String':
+            db_value = (item['db_value'])
+            set_string(data, offset, db_value, 256)
 
 def get_db_len(db_items,bytebit,datatype):
     offset_int = []
@@ -75,10 +97,12 @@ def main():
                 {
                     "name": x.split(space)[0],
                     "datatype": x.split(space)[1],
-                    "bytebit": x.split(space)[2]
+                    "bytebit": x.split(space)[2],
+                    "db_value": x.split(space)[3]
                 } for x in itemlist
             ]
             db_len = get_db_len(items, 'bytebit', 'datatype')
+            DBWrite(s71200, 1, db_len, items)
             DB_obj = DBRead(s71200, 1, db_len, items)
             print(DB_obj.DB001, DB_obj.DB002, DB_obj.DB003, DB_obj.DB004, DB_obj.DB005)
             sleep(5)
